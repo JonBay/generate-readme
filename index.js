4c4a169cb30ba2ccaa60a436a652a02c0d7671be
JonBay <134355923+JonBay@users.noreply.github.com>
2023-08-26 06:57:35 -0600

    generatemarkdown updates

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generatemarkdown = require('./utils/generateMarkdown');
 
+//call .prompt later.  For now just put the const questions in an array.  
 inquirer
   .prompt([
     {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
+        type: 'input',
         name: 'Title',
-        choices: ['email', 'phone', 'telekinesis'],
+        message: 'What is the title of your project?'
       },
     {
       type: 'input',
-      name: 'description',
-      message: 'What is your name?',
+      name: 'Description',
+      message: 'Give a brief description of your project.',
+    },
+    // {  this one will just be generated in the file.  
+    //   type: 'checkbox',
+    //   message: 'What languages do you know?',
+    //   name: 'TableofContents',
+    //   choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    // },
+    {
+      type: 'input',
+      name: 'Installation',
+      message: 'What are the instructions for installing your project?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'TableofContents',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      name: 'Usage',
+      message: 'Provide instructions and examples of use for this project.',
     },
     {
       type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'Installation',
-      choices: ['email', 'phone', 'telekinesis'],
+      name: 'License',
+      message: 'What license will you be using?',
+      choices: ['MIT', 'OtherLicenseA', 'OtherLicenseB'],
+    }, 
+    {
+      type: 'input',
+      name: 'Contributing',
+      message: 'List the git hub username of any contributors.  If none leave blank.',
+    },    
+    {
+      type: 'list',
+      name: 'Tests',
+      message: 'List any test instructions.',
     },
     {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'Usage',
-        choices: ['email', 'phone', 'telekinesis'],
-      },
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'License',
-        choices: ['email', 'phone', 'telekinesis'],
-      }, 
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'Contributing',
-        choices: ['email', 'phone', 'telekinesis'],
-      },    
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'Tests',
-        choices: ['email', 'phone', 'telekinesis'],
-      },
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'Questions',
-        choices: ['email', 'phone', 'telekinesis'],
-      },
+      type: 'input',
+      name: 'Username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'Email',
+      message: 'What is your e-mail address?',
+    },
   ])
   .then((data) => {
-    fs.writeFile('README.md', JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile('README.md', (generatemarkdown), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
 
 
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//function init() {}
+
+// Function call to initialize app
+//init();
